42bca7f249955b51d0d14e6b9d340b099bd10806
Adrian235713 <66631454+Adrian235713@users.noreply.github.com>
2021-03-24 20:36:11 +0100

    ruter done
    ruter done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import Setup from './tutorial/6-useReducer/setup/index'
+import Setup from './tutorial/11-react-router/setup'
 
 
 function App() {
-
-
 
   return (
     <div className='container'>
